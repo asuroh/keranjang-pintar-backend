c00997128be80d8775fb7102408d1be6e15820e9
thoriq aziz asuro <thoriq.azizasuro@gmail.com>
2021-06-26 14:19:07 +0700

    update db

--- files/db.sql
+++ files/db.sql
@@ -8,8 +8,5 @@ CREATE TABLE users (
   image_path text,
   created_at timestamp(6) DEFAULT now(),
   updated_at timestamp(6) DEFAULT now(),
-  deleted_at timestamp(6)
+  deleted_at timestamp(6) DEFAULT null
 );
-
-INSERT INTO users(id, name, email, password, image_path, created_at, updated_at, deleted_at) VALUES
-('0c334e29-609a-4c7d-856b-f004a1399b3b', 'thoriq aziz asuro', 'thoriq.azizasuro@gmail.com', '$2a$14$Zr20eYNB/D/q0InBRVcfgunahuUodXyLqCSJZE.FYONkBe6QXtUVm', '/c00p9aevvhfkgt1rtjg0_WhatsApp-Image-2021-01-14-at-19.28.21.jpeg', '2021-01-15 12:07:40.845362', '2021-01-15 13:12:09.801545', NULL);
