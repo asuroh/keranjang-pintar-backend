c845f079f778774e27efcfd12a639a66277e2b04
thoriq aziz asuro <thoriq.azizasuro@gmail.com>
2021-06-26 15:39:17 +0700

    add api product

--- files/db.sql
+++ files/db.sql
@@ -8,5 +8,30 @@ CREATE TABLE users (
   image_path text,
   created_at timestamp(6) DEFAULT now(),
   updated_at timestamp(6) DEFAULT now(),
-  deleted_at timestamp(6) DEFAULT null
+  deleted_at timestamp(6) 
+);
+
+DROP TABLE IF EXISTS categories; 
+CREATE TABLE categories ( 
+  id char(36) PRIMARY KEY,
+  name VARCHAR(30) not null, 
+  description text,
+  created_at timestamp(6) DEFAULT now(),
+  updated_at timestamp(6) DEFAULT now(),
+  deleted_at timestamp(6)
+);
+
+DROP TABLE IF EXISTS products;
+CREATE TABLE products ( 
+  id char(36) PRIMARY KEY,
+  category_id char(36) not null, 
+  code VARCHAR(20) not null, 
+  name VARCHAR(30) not null,
+  description text,
+  image_path text,
+  price DECIMAL DEFAULT 0,
+  qty int DEFAULT 0, 
+  created_at timestamp(6) DEFAULT now(),
+  updated_at timestamp(6) DEFAULT now(),
+  deleted_at timestamp(6) 
 );
